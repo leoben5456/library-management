2dde51db8fec73af27780c4b0d8629ada4d69240
taha <ELHAJHOUJ.TAHA@emsi-edu.com>
2024-12-02 00:36:40 +0100

    etudiant

---
+++ backend/user-service/src/main/java/com/example/userservice/Model/Etudiant.java
@@ -0,0 +1,28 @@
+package com.example.userservice.Model;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "etudiant")
+public class Etudiant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+    private String classe;
+    private String niveau;
+    private String specialite;
+    private String telephone;
+}
--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.userservice.Model")
 public class UserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
 }
